1cd2b04d44c1967bc4378b1bc8893c15d165632e
as ahmed <rakibasrr13@gmail.com>
2020-08-06 22:16:25 +0300

    true vs false

---
+++ true-false-Elements.js
@@ -0,0 +1,21 @@
+
+// Falsy:
+// false
+// 0
+// ""
+// udenfined
+// null
+// NaN
+
+// truthy
+// '0'," ",[]
+
+
+let name ="0";
+
+if (name) {
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
